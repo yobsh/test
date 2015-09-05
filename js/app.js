80badbde19c80be8c1425119f9e2d8d25bb59b23
Yobad Sayas <gen36.33@gmail.com>
2015-09-04 22:18:26 -0500

    fix minus 0

--- js/app.js
+++ js/app.js
@@ -7,9 +7,11 @@ $('#1000 .plus').click(function(){
     showResult ();
 });
 $('#1000 .minus').click(function(){
-  $('#1000 .ammount').text(parseInt($('#1000 .ammount').text())-1);
+  if ( parseInt($('#1000 .ammount').text()) > 0){
+    $('#1000 .ammount').text(parseInt($('#1000 .ammount').text())-1);
     suma = suma - 1000;
     showResult ();
+  }
 });
 
 //500 pesos
@@ -19,9 +21,11 @@ $('#500 .plus').click(function(){
     showResult ();
 });
 $('#500 .minus').click(function(){
-  $('#500 .ammount').text(parseInt($('#500 .ammount').text())-1);
+  if ( parseInt($('#500 .ammount').text()) > 0){
+    $('#500 .ammount').text(parseInt($('#500 .ammount').text())-1);
     suma = suma - 500;
     showResult ();
+  }
 });
 
 //200 pesos
@@ -31,9 +35,11 @@ $('#200 .plus').click(function(){
     showResult ();
 });
 $('#200 .minus').click(function(){
-  $('#200 .ammount').text(parseInt($('#200 .ammount').text())-1);
+  if ( parseInt($('#200 .ammount').text()) > 0){
+    $('#200 .ammount').text(parseInt($('#200 .ammount').text())-1);
     suma = suma - 200;
     showResult ();
+  }
 });
 
 //100 pesos
@@ -43,9 +49,11 @@ $('#100 .plus').click(function(){
     showResult ();
 });
 $('#100 .minus').click(function(){
-  $('#100 .ammount').text(parseInt($('#100 .ammount').text())-1);
+  if ( parseInt($('#100 .ammount').text()) > 0){
+    $('#100 .ammount').text(parseInt($('#100 .ammount').text())-1);
     suma = suma - 100;
     showResult ();
+  }
 });
 
 //50 pesos
@@ -55,9 +63,11 @@ $('#50 .plus').click(function(){
     showResult ();
 });
 $('#50 .minus').click(function(){
-  $('#50 .ammount').text(parseInt($('#50 .ammount').text())-1);
+  if ( parseInt($('#50 .ammount').text()) > 0){
+    $('#50 .ammount').text(parseInt($('#50 .ammount').text())-1);
     suma = suma - 50;
     showResult ();
+  }
 });
 
 //20 pesos
@@ -67,9 +77,11 @@ $('#20 .plus').click(function(){
     showResult ();
 });
 $('#20 .minus').click(function(){
-  $('#20 .ammount').text(parseInt($('#20 .ammount').text())-1);
+  if ( parseInt($('#20 .ammount').text()) > 0){
+    $('#20 .ammount').text(parseInt($('#20 .ammount').text())-1);
     suma = suma - 20;
     showResult ();
+  }
 });
 
 //10 pesos
@@ -79,9 +91,11 @@ $('#10 .plus').click(function(){
     showResult ();
 });
 $('#10 .minus').click(function(){
-  $('#10 .ammount').text(parseInt($('#10 .ammount').text())-1);
+  if ( parseInt($('#10 .ammount').text()) > 0){
+    $('#10 .ammount').text(parseInt($('#10 .ammount').text())-1);
     suma = suma - 10;
     showResult ();
+  }
 });
 
 //5 pesos
@@ -91,9 +105,11 @@ $('#5 .plus').click(function(){
     showResult ();
 });
 $('#5 .minus').click(function(){
-  $('#5 .ammount').text(parseInt($('#5 .ammount').text())-1);
+  if ( parseInt($('#5 .ammount').text()) > 0){
+    $('#5 .ammount').text(parseInt($('#5 .ammount').text())-1);
     suma = suma - 5;
     showResult ();
+  }
 });
 
 //2 pesos
@@ -103,9 +119,11 @@ $('#2 .plus').click(function(){
     showResult ();
 });
 $('#2 .minus').click(function(){
-  $('#2 .ammount').text(parseInt($('#2 .ammount').text())-1);
+  if ( parseInt($('#2 .ammount').text()) > 0){
+    $('#2 .ammount').text(parseInt($('#2 .ammount').text())-1);
     suma = suma - 2;
     showResult ();
+  }
 });
 
 //1 pesos
@@ -115,9 +133,11 @@ $('#1 .plus').click(function(){
     showResult ();
 });
 $('#1 .minus').click(function(){
-  $('#1 .ammount').text(parseInt($('#1 .ammount').text())-1);
+  if ( parseInt($('#1 .ammount').text()) > 0){
+    $('#1 .ammount').text(parseInt($('#1 .ammount').text())-1);
     suma = suma - 1;
     showResult ();
+  }
 });
 
 
@@ -128,20 +148,23 @@ $('#50c .plus').click(function(){
     showResult ();
 });
 $('#50c .minus').click(function(){
-  $('#50c .ammount').text(parseInt($('#50c .ammount').text())-1);
+  if ( parseInt($('#50c .ammount').text()) > 0){
+    $('#50c .ammount').text(parseInt($('#50c .ammount').text())-1);
     suma = suma - .5;
     showResult ();
+  }
 });
 
 
 //generic fuctions
 function showResult(){
-  $('#result .total').html('$'+suma)
+  $('#result .total').html('$'+suma);
 }
 
 $('#result i').click(function(){
     $('.ammount').text(0);
     $('#result .total').html('$0');
+    suma=0;
 });
 
 
