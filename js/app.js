1331b40651d49acebacec49357c38c4d589c2e4c
Yobad Sayas <gen36.33@gmail.com>
2015-09-04 22:08:35 -0500

    no message

--- js/app.js
+++ js/app.js
@@ -144,3 +144,25 @@ $('#result i').click(function(){
     $('#result .total').html('$0');
 });
 
+
+/* center modal */
+function centerModals($element) {
+  var $modals;
+  if ($element.length) {
+    $modals = $element;
+  } else {
+    $modals = $('.modal-vcenter:visible');
+  }
+  $modals.each( function(i) {
+    var $clone = $(this).clone().css('display', 'block').appendTo('body');
+    var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+    top = top > 0 ? top : 0;
+    $clone.remove();
+    $(this).find('.modal-content').css("margin-top", top);
+  });
+}
+$('.modal-vcenter').on('show.bs.modal', function(e) {
+  centerModals($(this));
+});
+$(window).on('resize', centerModals);
+
